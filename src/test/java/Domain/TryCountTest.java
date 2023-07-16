bc1c32b14653e5c487c0073942f0ab4d1e62d741
2384320 <chdms1549@gmail.com>
2023-07-17 00:36:44 +0900

    test: TryCount에 대한 테스트 추가

--- src/test/java/Domain/TryCountTest.java
+++ src/test/java/Domain/TryCountTest.java
@@ -5,9 +5,22 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class TryCountTest {
+    @DisplayName("시도할 횟수는 1 이상의 숫자로 작성해야 한다.")
+    @Test
+    public void tryCountRightTest() {
+        // given
+        final String testTryCount = "5";
+
+        // when
+        final TryCount tryCount = new TryCount(testTryCount);
+
+        // then
+        assertThat(tryCount.count).isEqualTo(Integer.parseInt(testTryCount));
+    }
 
     @DisplayName("시도할 횟수를 0회로 작성하면 예외가 발생한다.")
     @Test
