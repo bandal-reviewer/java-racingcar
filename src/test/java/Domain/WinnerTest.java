1ee7e6903d745232f82e083d365c26b9d46d38c9
2384320 <chdms1549@gmail.com>
2023-07-16 23:19:41 +0900

    test: 우승자를 구하는 테스트 추가

--- src/test/java/Domain/WinnerTest.java
+++ src/test/java/Domain/WinnerTest.java
@@ -24,4 +24,23 @@ public class WinnerTest {
         // then
         assertThat(winner.winner).isEqualTo(winnerList);
     }
+
+    @DisplayName("가장 많이 움직인 자동차가 우승한다.")
+    @Test
+    public void winnerTest3() {
+        // given
+        final Car car1 = new Car("메르세데스");
+        final Car car2 = new Car("아반떼");
+        final Car car3 = new Car("포르쉐");
+        final List<Car> cars = new ArrayList<>(List.of(car1, car2, car3));
+
+        // when
+        car2.move(4);
+        car3.move(4);
+        car3.move(4);
+        Winner winner = new Winner(cars);
+
+        // then
+        assertThat(winner.winner).isEqualTo(new ArrayList<>(List.of(car3)));
+    }
 }
