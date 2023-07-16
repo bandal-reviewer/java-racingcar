e76c33a9c8b37af57c9d399d87af29200b1152e1
2384320 <chdms1549@gmail.com>
2023-07-16 23:06:29 +0900

    test: 한 명 이상의 우승자를 담는 Winner 객체 테스트 추가

--- src/test/java/Domain/WinnerTest.java
+++ src/test/java/Domain/WinnerTest.java
@@ -1,20 +1,27 @@
 package Domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class WinnerTest {
-    @DisplayName("우승자는 이름을 가진다.")
+    @DisplayName("우승자는 한 명 이상일 수 있다.")
     @Test
-    public void winnerTest1() {
+    public void winnerTest2() {
         // given
-        final String winnerName = "메르세데스";
+        final Car car1 = new Car("메르세데스");
+        final Car car2 = new Car("아반떼");
+        final List<Car> winnerList = new ArrayList<>(Arrays.asList(car1, car2));
 
         // when
-        Winner winner = new Winner(winnerName);
+        Winner winner = new Winner(winnerList);
 
         // then
-        Assertions.assertThat(winner.name).isEqualTo(winnerName);
+        assertThat(winner.winner).isEqualTo(winnerList);
     }
 }
