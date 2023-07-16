79be1a6893392e7874f501590ff7fce33f454ded
2384320 <chdms1549@gmail.com>
2023-07-16 15:53:29 +0900

    test: 자동차 이름 길이 검증에 대한 테스트 추가

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class CarTest {
     @DisplayName("자동차는 이름을 가진다.")
@@ -16,5 +17,17 @@ public class CarTest {
 
         // then
         assertThat(car.name).isEqualTo(carName);
+    }
+
+    @DisplayName("자동차의 이름은 5자를 초과할 수 없다.")
+    @Test
+    public void carNameLengthTest() {
+        // given
+        final String carName = "Mercedes";
+
+        // when, then
+        assertThatThrownBy(() -> new Car(carName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[Error]");
     }
 }
