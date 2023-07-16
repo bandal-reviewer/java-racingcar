3a04f26db314e9f6074c09eb14610097f79901e8
2384320 <chdms1549@gmail.com>
2023-07-16 15:53:11 +0900

    feat: 자동차 이름 길이 검증 기능 추가

--- src/main/java/Domain/Car.java
+++ src/main/java/Domain/Car.java
@@ -4,6 +4,12 @@ public class Car {
     public final String name;
 
     public Car(String name) {
+        validateCarNameLength(name);
         this.name = name;
+    }
+
+    public void validateCarNameLength(String name) {
+        if (name.length() > 5)
+            throw new IllegalArgumentException("[Error] 자동차 이름은 5자를 초과할 수 없습니다.");
     }
 }
