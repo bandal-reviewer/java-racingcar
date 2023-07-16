8a388c72084ccb22da6d0844c3f188a6a6f05e3b
2384320 <chdms1549@gmail.com>
2023-07-16 16:10:52 +0900

    feat: 자동차 전진 기능 추가

--- src/main/java/Domain/Car.java
+++ src/main/java/Domain/Car.java
@@ -3,11 +3,15 @@ package Domain;
 public class Car {
     private static final int MAX_CAR_NAME_LENGTH = 5;
     private static final int MIN_CAR_NAME_LENGTH = 1;
+    private static final int CAR_MOVE_LENGTH = 1;
+
     public final String name;
+    public int position;
 
     public Car(String name) {
         validateCarNameLength(name);
         this.name = name;
+        this.position = 0;
     }
 
     public void validateCarNameLength(String name) {
@@ -15,5 +19,9 @@ public class Car {
         if (nameLength > MAX_CAR_NAME_LENGTH
                 || nameLength < MIN_CAR_NAME_LENGTH)
             throw new IllegalArgumentException("[Error] 자동차 이름은 5자를 초과할 수 없습니다.");
+    }
+
+    public void move() {
+        this.position += CAR_MOVE_LENGTH;
     }
 }
