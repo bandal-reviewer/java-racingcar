27cf348ed4911bf5fe8ef8baaa814662b6aa7be9
2384320 <chdms1549@gmail.com>
2023-07-16 19:28:44 +0900

    test: 자동차 이름을 쉼표로 구분할 수 있는 테스트 수정
    - String 리스트가 아닌 Car 리스트로 비교하도록 변경

--- src/test/java/Domain/CarsTest.java
+++ src/test/java/Domain/CarsTest.java
@@ -1,6 +1,5 @@
 package Domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,24 +7,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CarsTest {
+    private final Car car = new Car();
 
     @DisplayName("자동차 이름은 쉼표(,)로 구분할 수 있다.")
     @Test
     public void carsTest1() {
         // given
         final String carNames = "메르세데스,포르쉐,아반떼";
-        final List<String> carNameList = new ArrayList<>(Arrays.asList(
-                "메르세데스",
-                "포르쉐",
-                "아반떼"
+        final List<Car> carNameList = new ArrayList<>(Arrays.asList(
+                car.from("메르세데스"),
+                car.from("포르쉐"),
+                car.from("아반떼")
         ));
-        final Cars cars = new Cars();
 
         // when
-        List<String> resultCarList = cars.convertCarNamesToList(carNames);
+        final Cars cars = new Cars(carNames);
 
         // then
-        Assertions.assertThat(resultCarList).isEqualTo(carNameList);
+        assertThat(cars.getCars()).isEqualTo(carNameList);
     }
 }
