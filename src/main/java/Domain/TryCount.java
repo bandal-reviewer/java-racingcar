8d2f2c914144e09a0756160623bfc0540251e73d
2384320 <chdms1549@gmail.com>
2023-07-16 19:46:27 +0900

    feat: TryCount 객체 생성
    - 0을 입력하면 예외 발생 기능 추가

---
+++ src/main/java/Domain/TryCount.java
@@ -0,0 +1,25 @@
+package Domain;
+
+public class TryCount {
+    final int count;
+
+    public TryCount(String count) {
+        //validateCountFormat(count);
+        validateCountRange(count);
+        this.count = Integer.parseInt(count);
+    }
+
+//    public void validateCountFormat(String count) {
+//        if (count.matches("[0-9]*"))
+//            throw new IllegalArgumentException("[Error] 시도할 횟수는 숫자로 작성해야 합니다.");
+//    }
+
+    public void validateCountRange(String count) {
+        if (Integer.parseInt(count) == 0)
+            throw new IllegalArgumentException("[Error] 시도할 횟수는 1회 이상으로 작성해야 합니다.");
+    }
+
+//    public int getCount() {
+//        return count;
+//    }
+}
