118dbe69fb7792b0004409778944b94a17f6a808
2384320 <chdms1549@gmail.com>
2023-07-16 19:12:44 +0900

    feat: Car 객체 정적 팩토리 메소드화

--- src/main/java/Domain/Car.java
+++ src/main/java/Domain/Car.java
@@ -1,15 +1,21 @@
 package Domain;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class Car {
     private static final int MAX_CAR_NAME_LENGTH = 5;
     private static final int MIN_CAR_NAME_LENGTH = 1;
     private static final int CAR_MOVE_LENGTH = 1;
     private static final int MIN_RANDOM_NUMBER = 4;
 
+    private final Map<String, Car> carCache = new HashMap<>();
+
     public final String name;
     public int position;
 
-    public Car(String name) {
+    private Car(String name) {
         validateCarNameLength(name);
         this.name = name;
         this.position = 0;
@@ -25,5 +31,28 @@ public class Car {
     public void move(int randomNumber) {
         if (randomNumber >= MIN_RANDOM_NUMBER)
             this.position += CAR_MOVE_LENGTH;
+    }
+
+    public Car from(String name) {
+        if (carCache.containsKey(name))
+            return carCache.get(name);
+        else {
+            Car car = new Car(name);
+            carCache.put(name, car);
+            return car;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(this.name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
