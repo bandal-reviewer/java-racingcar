a8e90241c73a2943fa6f1363e5cd2f1c82279b12
2384320 <chdms1549@gmail.com>
2023-07-16 19:53:29 +0900

    test: 시도할 횟수 형식에 따른 검증 기능 테스트 추가

--- src/test/java/Domain/TryCountTest.java
+++ src/test/java/Domain/TryCountTest.java
@@ -2,6 +2,8 @@ package Domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
@@ -13,6 +15,18 @@ public class TryCountTest {
         // given
         final String testTryCount = "0";
 
+        // when
+        // then
+        assertThatThrownBy(() -> new TryCount(testTryCount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[Error]");
+    }
+
+    @DisplayName("시도할 횟수를 숫자로 입력하지 않으면 예외가 발생한다.")
+    @ValueSource(strings = {"three", "", "세번"})
+    @ParameterizedTest
+    public void tryCountFormatTest(String testTryCount) {
+        // given
         // when
         // then
         assertThatThrownBy(() -> new TryCount(testTryCount))
