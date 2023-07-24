cfb2d45db75d5d734e67ef8f6f6f2eafcd896930
2384320 <chdms1549@gmail.com>
2023-07-24 21:26:41 +0900

    test: 한 명 우승에 대한 테스트 추가

--- src/test/java/Domain/WinnerTest.java
+++ src/test/java/Domain/WinnerTest.java
@@ -10,6 +10,24 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WinnerTest {
+    @DisplayName("우승자는 가장 많이 움직인 자동차가 된다.")
+    @Test
+    public void winnerTest() {
+        // given
+        final Car car1 = new Car("메르세데스");
+        final Car car2 = new Car("아반떼");
+        final Car car3 = new Car("포르쉐");
+        final List<Car> carList = new ArrayList<>(Arrays.asList(car1, car2, car3));
+        final List<Car> winnerList = new ArrayList<>(List.of(car1));
+        car1.move(4);
+
+        // when
+        Winner winner = new Winner(carList);
+
+        // then
+        assertThat(winner.winner).isEqualTo(winnerList);
+    }
+
     @DisplayName("우승자는 한 명 이상일 수 있다.")
     @Test
     public void winnerMoreThanOneTest() {
