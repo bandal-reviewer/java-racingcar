1256bcf4b1e08b7a26e38cabcd80248e41525c25
2384320 <chdms1549@gmail.com>
2023-07-24 21:05:43 +0900

    refactor: 랜덤 숫자 범위 상수화

--- src/main/java/Domain/RandomNumber.java
+++ src/main/java/Domain/RandomNumber.java
@@ -3,8 +3,10 @@ package Domain;
 import java.util.Random;
 
 public class RandomNumber {
+    private static final int MAX_RANDOM_NUMBER_RANGE = 10;
+
     public static int generateRandomNumber() {
         Random random = new Random();
-        return random.nextInt(10);
+        return random.nextInt(MAX_RANDOM_NUMBER_RANGE);
     }
 }
